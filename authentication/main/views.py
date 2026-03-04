3b8fa4996a1812c00f1bada302713040b50d5095
Joshkovu <joashkuteesa223@gmail.com>
2026-03-04 19:37:55 +0300

    feat:implement signup user registration

--- authentication/main/views.py
+++ authentication/main/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
     return render(request, "main/home.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        # if form.is_valid():
+        #     user = form.save()
+        #     login(request, user)
+        #     return redirect("home")
+    else:
+        form = SignUpForm()
+    return render(request, "registration/sign_up.html", {"form": form})
