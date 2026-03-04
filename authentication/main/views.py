2592a99034ef619091e439754481bead400c9f78
Joshkovu <joashkuteesa223@gmail.com>
2026-03-04 19:42:36 +0300

    refactor: update signup view and form

--- authentication/main/views.py
+++ authentication/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from .forms import SignUpForm
+from django.contrib.auth import login , logout, authenticate
 # Create your views here.
 def home(request):
     return render(request, "main/home.html")
@@ -8,10 +9,10 @@ def home(request):
 def signup(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
-        # if form.is_valid():
-        #     user = form.save()
-        #     login(request, user)
-        #     return redirect("home")
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/home")
     else:
         form = SignUpForm()
     return render(request, "registration/sign_up.html", {"form": form})
