52364ad23f8630913d72df7b7a84c1bc6c356b9f
Joshkovu <joashkuteesa223@gmail.com>
2026-03-04 21:58:00 +0300

    refactor:adjust the code to use forms and add comment functionality

--- authentication/main/views.py
+++ authentication/main/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import redirect, render
 from .forms import CommentForm, SignUpForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login , logout, authenticate
+from .models import Comment
 # Create your views here.
 @login_required(login_url="/login")
 def home(request):
-    return render(request, "main/home.html")
+    if request.method == "POST":
+        comment_id = request.POST.get("comment_id")
+        comment = Comment.objects.filter(id=comment_id, user=request.user).first()
+        if comment and comment.user == request.user:
+            comment.delete()
+        try:
+            comment = Comment.objects.get(id=comment_id, user=request.user)
+            comment.delete()
+            return redirect("/home/")
+        except Comment.DoesNotExist:
+            return redirect("/home/")
+    return render(request, "main/home.html",{"comments": Comment.objects.all().order_by("-created_at")})
 
 @login_required(login_url="/login")
 def create_comment(request):
@@ -15,7 +27,7 @@ def create_comment(request):
             comment =form.save(commit=False)
             comment.user = request.user
             comment.save()
-            return redirect("/home")
+            return redirect("/home/")
     else:
         form = CommentForm()
     return render(request, "main/create_comment.html", {"form": form})
@@ -27,7 +39,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("/home")
+            return redirect("/home/")
     else:
         form = SignUpForm()
     return render(request, "registration/sign_up.html", {"form": form})
